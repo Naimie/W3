c79fb7e855a520a0e8ea7cde15dc7d4d84f947fb
Naimie <Kristinaelmgren@gmail.com>
2019-02-16 23:57:04 +0100

    5.2 done but not comments or tests.

--- pmap_any.erl
+++ pmap_any.erl
@@ -5,26 +5,31 @@
 smap(_, []) -> [];
 smap(F, [H|T]) -> [F(H) | smap(F, T)].
 
+fib(0)-> 0;
 fib(1) -> 1;
 fib(2) -> 1;
 fib(N) -> fib(N-1) + fib(N-2).
 
 pmap(F, L) ->
   S = self(),
-  Pids = smap(fun(I) ->
-    spawn(fun() -> do_f(S, F, I) end)
+  Ref = erlang:make_ref(),
+  lists:foreach(fun(I) ->
+    spawn(fun() -> do_f(S, Ref, F, I) end)
               end, L),
 %% gather the results
-  gather(Pids).
+  report(length(L),Ref,[]).
 
-do_f(Parent, F, I) ->
-  Parent ! {self(), (catch F(I))}.
 
-gather([Pid|T]) ->
+do_f(Parent, Ref, F, I) ->
+  Parent ! { Ref, (catch F(I))}.
+
+report(0, _, L)->
+  %the first computed function will be put in the list and the next value will be attached at the beginning. To get it in computation order it needs to be reversed.
+  lists:reverse(L);
+
+report(N,Ref,L)->
   receive
-    {Pid, Ret} -> [Ret|gather(T)]
-  end;
-
-
-gather([]) ->
-  [].
+    {Ref,Ret} ->
+      io:format("~p~n",[{Ref,Ret}]),
+      report(N-1,Ref, [Ret|L])
+  end.
