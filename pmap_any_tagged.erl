bc1d15adcfaf586c9f89e9ea3d9eb5df4409a36d
Naimie <Kristinaelmgren@gmail.com>
2019-02-17 00:46:36 +0100

    5.3 working but not sure if correct.

--- pmap_any_tagged.erl
+++ pmap_any_tagged.erl
@@ -1,4 +1,4 @@
--module(pmap_any).
+-module(pmap_any_tagged).
 -compile(export_all).
 
 
@@ -10,26 +10,30 @@ fib(1) -> 1;
 fib(2) -> 1;
 fib(N) -> fib(N-1) + fib(N-2).
 
-pmap_any(F, L) ->
+pmap_any_tagged(F, L) ->
   S = self(),
   Ref = erlang:make_ref(),
   lists:foreach(fun(I) ->
     spawn(fun() -> do_f(S, Ref, F, I) end)
-              end, L),
+                end, L),
 %% gather the results
-  report(length(L),Ref,[]).
+  report(length(L),Ref,[],L).
 
 
 do_f(Parent, Ref, F, I) ->
   Parent ! { Ref, (catch F(I))}.
 
-report(0, _, L)->
+report(0, _, L1, L2)->
   %the first computed function will be put in the list and the next value will be attached at the beginning. To get it in computation order it needs to be reversed.
-  lists:reverse(L);
+  Input_list = lists:sort(L2),
+  Fib_list = lists:reverse(L1),
+  io:format("~p~n",[{Input_list, Fib_list}]),
+  L3 = lists:zip(Input_list,Fib_list),
+  L3;
 
-report(N,Ref,L)->
+report(N,Ref,L1, L2)->
   receive
     {Ref,Ret} ->
-      io:format("~p~n",[{Ref,Ret}]),
-      report(N-1,Ref, [Ret|L])
+      %io:format("~p~n",[{Ref,Ret}]),
+      report(N-1,Ref, [Ret|L1], L2)
   end.
